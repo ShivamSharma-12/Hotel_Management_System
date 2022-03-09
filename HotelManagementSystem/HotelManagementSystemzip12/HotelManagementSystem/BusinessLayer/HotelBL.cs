0793365c6e0a694f4f58f5baf1af8499d6a83dd3
Shivam Sharma <85987637+ShivamSharma-12@users.noreply.github.com>
2022-03-09 23:48:27 +0530

    Add files via upload

---
+++ HotelManagementSystem/HotelManagementSystemzip12/HotelManagementSystem/BusinessLayer/HotelBL.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject;
+using DataAcessLayer;
+
+namespace BusinessLayer
+{
+
+    public class CustomerBL
+    {
+
+        public DataSet GetCityBL() // passing Bussiness object Here
+        {
+            try
+            {
+                HotelDAL objUserda = new HotelDAL(); // Creating object of Dataccess
+                return objUserda.hh(); // calling Method of DataAccess
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public DataSet GetHotelByCityBL(string cityName) // passing Bussiness object Here
+        {
+            try
+            {
+                HotelDAL objUserda = new HotelDAL(); // Creating object of Dataccess
+                return objUserda.searchcityBl(cityName); // calling Method of DataAccess
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public DataSet GetRoomTypeByHotelNameBL(string HotelName) // passing Bussiness object Here
+        {
+            try
+            {
+                HotelDAL objUserda = new HotelDAL(); // Creating object of Dataccess
+                return objUserda.searchRoomTypeBl( HotelName); // calling Method of DataAccess
+            }
+            catch
+            {
+                throw;
+
+            }
+
+        }
+        public DataSet GetRoomDetails(string HotelName, string RoomType) // passing Bussiness object Here
+        {
+            try
+            {
+                HotelDAL objUserda = new HotelDAL(); // Creating object of Dataccess
+                return objUserda.SearchByRoomType(HotelName,RoomType); // calling Method of DataAccess
+            }
+            catch
+            {
+                throw;
+
+            }
+
+        }
+
+        public int UpdateRates(int RatePerNight, int RoomTypeId, int HotelId) // passing Bussiness object Here
+        {
+            try
+            {
+                HotelDAL objUserda = new HotelDAL(); // Creating object of Dataccess
+                return objUserda.UpdateRate(RatePerNight, RoomTypeId, HotelId); // calling Method of DataAccess
+            }
+            catch
+            {
+                throw;
+
+            }
+
+        }
+
+    }
+}
